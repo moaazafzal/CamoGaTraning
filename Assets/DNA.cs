77b189e70c9f7e3f5599836f6f5feaa1a2923039
moaaz <moaazafzal.1111@gmail.com>
2019-09-09 16:15:49 +0500

    Initial commit

---
+++ Assets/DNA.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DNA : MonoBehaviour
+{
+    public static DNA instance;
+    public float r,g,b;
+    bool dead = false;
+    public float timeToDie = 0;
+    SpriteRenderer sRendere;
+    Collider2D sCollider;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        instance = this;
+    }
+    void Start()
+    {
+       
+           sRendere = GetComponent<SpriteRenderer>();
+        sCollider = GetComponent<Collider2D>();
+
+        sRendere.color = new Color(r, g, b);
+    }
+    public void playerColor()
+    {
+       
+    }
+    private void OnMouseDown()
+    {
+        dead = true;
+        timeToDie = PopulationManager.elapsed;
+        sRendere.enabled = false;
+        sCollider.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/PopulationManager.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class PopulationManager : MonoBehaviour
+{
+    public GameObject personPrefab;
+    public int populationSize = 10;
+    public List<GameObject> population = new List<GameObject>();
+    // Start is called before the first frame update
+    public static float elapsed = 0;
+    int trailTime = 10;
+    int generation = 1;
+    GUIStyle guiStyle = new GUIStyle();
+    private void OnGUI()
+    {
+        guiStyle.fontSize = 50;
+        guiStyle.normal.textColor = Color.white;
+        GUI.Label(new Rect(10, 10, 100, 20), "Gemeration: " + generation, guiStyle);
+        GUI.Label(new Rect(10, 10, 100, 20), "Trial Time: " + (int)elapsed, guiStyle);
+
+
+    }
+    void Start()
+    {
+        for(int i=0;i<populationSize;i++)
+        {
+            Vector3 pos = new Vector3(Random.Range(-9f, 9f), Random.Range(-3.5f, 3.5f), 0);
+            GameObject go = Instantiate(personPrefab, pos, Quaternion.identity);
+            go.GetComponent<DNA>().r = Random.Range(0.0f, 1.0f);
+            go.GetComponent<DNA>().g = Random.Range(0.0f, 1.0f);
+            go.GetComponent<DNA>().b = Random.Range(0.0f, 1.0f);
+            population.Add(go);
+        }
+       // DNA.instance.playerColor();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsed += Time.deltaTime;
+        if(elapsed>trailTime)
+        {
+            BreednewPopulation();
+            elapsed = 0;
+        }
+    }
+    public void BreednewPopulation()
+    {
+        List<GameObject> newPopulation = new List<GameObject>();
+        List<GameObject> sortedList = population.OrderBy(o => o.GetComponent<DNA>().timeToDie).ToList();
+        population.Clear();
+        for(int i=(int)(sortedList.Count/2.0f)-1; i<sortedList.Count-1; i++)
+        {
+            population.Add(Breed(sortedList[i], sortedList[i + 1]));
+            population.Add(Breed(sortedList[i+1], sortedList[i ]));
+        }
+        for(int i=0;i<sortedList.Count;i++)
+        {
+            Destroy(sortedList[i]);
+        }
+        generation++;
+    }
+    GameObject Breed(GameObject parent1,GameObject parent2)
+    {
+        Vector3 pos = new Vector3(Random.Range(-9f, 9f), Random.Range(-3.5f, 3.5f), 0);
+        GameObject offspring = Instantiate(personPrefab, pos, Quaternion.identity);
+        DNA dna1 = parent1.GetComponent<DNA>();
+        DNA dna2 = parent2.GetComponent<DNA>();
+        offspring.GetComponent<DNA>().r = Random.Range(0, 10) < 5 ? dna1.r : dna2.r;
+        offspring.GetComponent<DNA>().g = Random.Range(0, 10) < 5 ? dna1.g : dna2.g;
+        offspring.GetComponent<DNA>().b = Random.Range(0, 10) < 5 ? dna1.b : dna2.b;
+        return offspring;
+
+
+    }
+
+
+}
